c197c04715eebb069519d6a36a290f0a3000db8f
Tyler Allen <t.allen@uncc.edu>
2024-05-09 14:37:30 -0400

    adding more registers

--- Makefile
+++ Makefile
@@ -1,2 +1,3 @@
 main: main.c
-	nvcc main.c -o out -I/usr/include/x86_64-linux-gnu -L/usr/lib/x86_64-linux-gnu -lunwind
+	gcc -O0 -g  main.c -o out -I/usr/include/x86_64-linux-gnu -L/usr/lib/x86_64-linux-gnu -lunwind
+	gcc -O0 -g  main.c -S -I/usr/include/x86_64-linux-gnu -L/usr/lib/x86_64-linux-gnu -lunwind
--- main.c
+++ main.c
@@ -6,10 +6,18 @@
 void bar(int, int);
 
 void foo(int a, int b) {
+    bar(a, b);
     int c = a + b;
     int d = a * b;
-    bar(c, d);
     return;
+}
+
+void print_reg(unw_cursor_t* cursor, long rid, unw_word_t reg, char* word)
+{
+        unw_get_reg(cursor, rid, &reg);
+//        printf("%s is %ld\n", word, reg);
+        printf("%s is %lx\n", word, (unsigned long)(reg & 0xFFFFFFFF));
+
 }
 
 void baz(void) {
@@ -30,10 +38,25 @@ void baz(void) {
         }
 
         if ((unw_get_proc_name(&cursor, functionName, sizeof(functionName), &offset) == 0)) {
+            printf("func: %s\n", functionName);
             printf("Frame %d: %s (+0x%lx)\n", frame, functionName, offset);
-            unw_get_reg(&cursor, UNW_X86_64_RDX, &reg);
-            int* x = (int*)reg;
-            printf("rsi is %d\n", reg);
+            print_reg(&cursor, UNW_X86_64_RDI, reg, "RDI");
+            print_reg(&cursor, UNW_X86_64_RSI, reg, "RSI");
+            print_reg(&cursor, UNW_X86_64_RDX, reg, "RDX");
+            print_reg(&cursor, UNW_X86_64_RCX, reg, "RCX");
+            print_reg(&cursor, UNW_X86_64_RAX, reg, "RAX");
+            print_reg(&cursor, UNW_X86_64_R8, reg, "R8");
+            print_reg(&cursor, UNW_X86_64_R9, reg, "R9");
+            print_reg(&cursor, UNW_X86_64_RBX, reg, "RBX");
+            print_reg(&cursor, UNW_X86_64_RBP, reg, "RBP");
+            print_reg(&cursor, UNW_X86_64_RSP, reg, "RSP");
+            print_reg(&cursor, UNW_X86_64_R10, reg, "R10");
+            print_reg(&cursor, UNW_X86_64_R11, reg, "R11");
+            print_reg(&cursor, UNW_X86_64_R12, reg, "R12");
+            print_reg(&cursor, UNW_X86_64_R13, reg, "R13");
+            print_reg(&cursor, UNW_X86_64_R14, reg, "R14");
+            print_reg(&cursor, UNW_X86_64_R15, reg, "R15");
+            print_reg(&cursor, UNW_X86_64_RIP, reg, "RIP");
         } else {
             printf("Frame %d: -- unknown function --\n", frame);
         }
@@ -43,9 +66,11 @@ void baz(void) {
 
 }
 
+
+
 void bar(int a, int b) {
     baz();
-    printf("%d", a-b);  
+    printf("%d\n", a-b);  
 }
 
 int main() {
